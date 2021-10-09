bd4a011c116a92e1a48e6ab915a846312db53a31
Alejandro Ramirez <alejandroramirez688@gmail.com>
2021-10-09 17:02:29 -0500

    deploy

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handlers');
 
 const app = express();
-const port = 3001;
+const port = processs.env.PORT || 3001;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
